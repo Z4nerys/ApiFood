77a9b67e56fea9ea8e50ea76b72776bb906dc5bb
zanerys <gmfkratze@gmail.com>
2022-02-21 23:52:31 -0300

    btn comprar, vender. faltan los detalles

--- src/components/food/FoodCard.js
+++ src/components/food/FoodCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const FoodCard = ({
     id,
@@ -8,17 +8,25 @@ export const FoodCard = ({
     origin,
     status
 }) => {
-    const handleBuy = () =>{
-        alert('soy ' + name)
+    const [buy, setBuy] = useState(false)
+
+    const handleBuy = () => {
+        setBuy(!buy)
     }
+
     return (
         <div className='col-md-4 col-sm-5 col-lg-3 mb-4'>
             <div className="card" >
                 <h5 style={{ height: "3rem" }} className='mt-4 card-title'>{name}</h5>
                 <img className="card-img-top" src={image} alt={title} />
                 <p style={{ height: "3rem" }} className="mt-4 text-muted">origin: {origin.name}</p>
-                <p className='card-footer'>status: {status}</p>
-                <button className="btn btn-dark" onClick={handleBuy}>Buy</button>
+                <p className='card-text'>status: {status}</p>
+                <button className='btn btn-warning mb-2'>ver detalles</button>
+                {buy ?
+                    <button className="btn btn-primary" onClick={handleBuy}>Buy</button>
+                    :
+                    <button className="btn btn-dark" onClick={handleBuy}>eliminar</button>
+                }
             </div>
         </div>
     )
